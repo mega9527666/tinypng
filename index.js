74197ee0181329a7d3f0861c4b3cd0970665de12
mega@mac <mega@mac.com>
2024-09-20 12:41:22 +0800

    fix

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 const tinify = require("tinify");
 const fs = require("fs");
+const path = require("path");
 
 tinify.key = "dGv62vCgBMNzlR4jZmSXrTYgZQ56CSM0";
 // tinify.key = "XYxRtw4lml028C0w9NXJDrj5k8TGqsgs";
 
 
-// const source = tinify.fromFile("assets");
-// source.toFile("optimized.webp");
-
-fs.readFile("assets/bg.jpg", (err, sourceData) => {
-    if (err) throw err;
-    tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
-        if (err) throw err;
-        // ...
-        console.log("resultData==", resultData);
-        fs.writeFile("bg.jpg", resultData, (err) => {
-            if (err) {
-                console.error("压缩文件失败", err);
+let compressPath = "./assets"
+function compressDir(dir) {
+    let stat = fs.statSync(dir);
+    if (!stat.isDirectory()) {
+        return;
+    }
+    var subpaths = fs.readdirSync(dir),
+        subpath, size, md5, compressed, relative;
+    for (var i = 0; i < subpaths.length; ++i) {
+        if (subpaths[i][0] === '.') {
+            continue;
+        }
+        subpath = path.join(dir, subpaths[i]);
+        stat = fs.statSync(subpath);
+        if (stat.isDirectory()) {
+            compressDir(subpath)
+        } else if (stat.isFile()) {
+            console.log("subpath=", subpath, stat)
+            let localLowerPath = subpath.toLocaleLowerCase();
+            if (localLowerPath.includes(".png") || localLowerPath.includes(".jpg")
+                || localLowerPath.includes(".jpeg") || localLowerPath.includes("webp")) {
+                fs.readFile(subpath, (err, sourceData) => {
+                    if (err) throw err;
+                    tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
+                        if (err) throw err;
+                        fs.writeFile(subpath, resultData, (err) => {
+                            if (err) {
+                                console.error("压缩文件失败", err);
+                            } else {
+                                console.log("压缩成功", subpath)
+                            }
+                        });
+                    });
+                });
             }
-            // console.log("writeddd==", err, dad);
-        });
-    });
-});
+        }
+    }
+}
+compressDir(compressPath);
+
